e80af7100709129b60533c823d96e24c4efd992e
Jonathan Lansey <lansey+github@gmail.com>
2019-08-05 22:57:28 -0400

    added tests for unless functionality

--- Swona/Tests/SwonaTests/ParserTests.swift
+++ Swona/Tests/SwonaTests/ParserTests.swift
@@ -6,53 +6,65 @@ final class ParserTests: XCTestCase {
     func testVariables() throws {
         try assertParseExpression(source: "foo", expected: "[Ref foo]")
     }
-    
+
     func testLiterals() throws {
         try assertParseExpression(source: "42", expected: "[Lit 42]")
         try assertParseExpression(source: "\"foo\"", expected: "[Lit \"foo\"]")
         try assertParseExpression(source: "true", expected: "[Lit true]")
     }
-    
-    
+
+
     func testIfStatements() throws {
         try assertParseExpression(source: "if (x) y else z", expected: "[If [Ref x] [Ref y] [Ref z]]")
         try assertParseExpression(source: "if (x) y", expected: "[If [Ref x] [Ref y] []]")
     }
-    
-    
+
+
+    func testUnlessStatements() throws {
+        try assertParseExpression(source: "unless (x) y else z", expected: "[If [Not [Ref x]] [Ref y] [Ref z]]")
+        try assertParseExpression(source: "unless (x) y", expected: "[If [Not [Ref x]] [Ref y] []]")
+    }
+
+
     func testWhileStatements() throws {
         try assertParseExpression(source: "while (x) y", expected: "[While [Ref x] [Ref y]]")
     }
-    
-    
+
+
     func testAssignment() throws {
         try assertParseExpression(source: "foo = bar", expected: "[Assign foo [Ref bar]]")
     }
-    
+
     func testVars() throws {
         try assertParseExpression(source: "var foo = bar", expected: "[Var foo [Ref bar]]")
     }
-    
+
     func testVals() throws {
         try assertParseExpression(source: "val foo = bar", expected: "[Val foo [Ref bar]]")
     }
-    
+
     func testIfAsAnExpression() throws {
         try assertParseExpression(source: "1 + if (true) 2 else 3", expected: "[Plus [Lit 1] [If [Lit true] [Lit 2] [Lit 3]]]")
         try assertParseExpression(source: "if (true) 2 else 3 + 4", expected: "[If [Lit true] [Lit 2] [Plus [Lit 3] [Lit 4]]]")
         try assertParseExpression(source: "(if (true) 2 else 3) + 4", expected: "[Plus [If [Lit true] [Lit 2] [Lit 3]] [Lit 4]]")
     }
-    
+
+    func testUnlessAsAnExpression() throws {
+        try assertParseExpression(source: "1 + unless (true) 2 else 3", expected: "[Plus [Lit 1] [If [Not [Lit true]] [Lit 2] [Lit 3]]]")
+        try assertParseExpression(source: "unless (true) 2 else 3 + 4", expected: "[If [Not [Lit true]] [Lit 2] [Plus [Lit 3] [Lit 4]]]")
+        try assertParseExpression(source: "(unless (true) 2 else 3) + 4", expected: "[Plus [If [Not [Lit true]] [Lit 2] [Lit 3]] [Lit 4]]")
+    }
+
     func testExpressionList() throws {
         try assertParseExpression(source: "{}", expected: "[ExpressionList []]")
         try assertParseExpression(source: "{ x; y; z }", expected: "[ExpressionList [[Ref x], [Ref y], [Ref z]]]")
     }
-    
+
     func testAssignmentToLiteralIsSyntaxError() throws {
         assertSyntaxError(code: "1 = bar;")
     }
-    
-    
+
+
     func testBinaryOperators() throws {
         try assertParseExpression(source: "1 + 2", expected: "[Plus [Lit 1] [Lit 2]]")
         try assertParseExpression(source: "1 - 2", expected: "[Minus [Lit 1] [Lit 2]]")
@@ -65,12 +77,12 @@ final class ParserTests: XCTestCase {
         try assertParseExpression(source: "true && false", expected: "[And [Lit true] [Lit false]]")
         try assertParseExpression(source: "true || false", expected: "[Or [Lit true] [Lit false]]")
     }
-    
+
     func testNot() throws {
         try assertParseExpression(source: "!x", expected: "[Not [Ref x]]")
     }
 
-    
+
     func testOperatorPrecedence() throws {
         try assertParseExpression(source: "a + b == c + d", expected: "[== [Plus [Ref a] [Ref b]] [Plus [Ref c] [Ref d]]]")
         try assertParseExpression(source: "a + (b == c) + d", expected: "[Plus [Plus [Ref a] [== [Ref b] [Ref c]]] [Ref d]]")
@@ -80,19 +92,19 @@ final class ParserTests: XCTestCase {
         try assertParseExpression(source: "a == b < c", expected: "[== [Ref a] [< [Ref b] [Ref c]]]")
         try assertParseExpression(source: "a == b || c == d && e == f", expected: "[Or [== [Ref a] [Ref b]] [And [== [Ref c] [Ref d]] [== [Ref e] [Ref f]]]]")
     }
-    
+
     func testFunctionCall() throws {
         try assertParseExpression(source: "foo()", expected: "[Call [Ref foo] []]")
         try assertParseExpression(source: "bar(1)", expected: "[Call [Ref bar] [[Lit 1]]]")
         try assertParseExpression(source: "baz(1, x)", expected: "[Call [Ref baz] [[Lit 1], [Ref x]]]")
         try assertParseExpression(source: "(baz)()", expected: "[Call [Ref baz] []]")
     }
-    
+
     func testFunctionDefinition() throws {
         try assertParseFunctionDefinition(source: "fun square(x: Int, y: Int): Int = x * x",
                                       expected: "FunctionDefinition(name=square, args=[(x, Int), (y, Int)], returnType=Int, body=[Multiply [Ref x] [Ref x]])")
     }
-    
+
     func testSamples() throws {
         try assertParseExpression(source:
             """
@@ -102,7 +114,7 @@ final class ParserTests: XCTestCase {
             """
             [If [== [Ref x] [Plus [Lit 2] [Lit 2]]] [ExpressionList [[Var t [Lit "It"]], [Assign s [Plus [Ref t] [Lit " worked!"]]]]] []]
             """)
-        
+
         try assertParseFunctionDefinition(source:
             """
             fun fib(i: Int): Int = if (i == 0 || i == 1)
@@ -114,7 +126,7 @@ final class ParserTests: XCTestCase {
             """
             FunctionDefinition(name=fib, args=[(i, Int)], returnType=Int, body=[If [Or [== [Ref i] [Lit 0]] [== [Ref i] [Lit 1]]] [Ref i] [Plus [Call [Ref fib] [[Minus [Ref i] [Lit 1]]]] [Call [Ref fib] [[Minus [Ref i] [Lit 2]]]]]])
             """)
-        
+
         try assertParseFunctionDefinition(source:
             """
             fun square(x: Int) = x * x
@@ -123,7 +135,7 @@ final class ParserTests: XCTestCase {
             """
             FunctionDefinition(name=square, args=[(x, Int)], returnType=null, body=[Multiply [Ref x] [Ref x]])
             """)
-        
+
         try assertParseFunctionDefinition(source:
             """
             fun cube(x: Int) = x * x * x
@@ -133,7 +145,7 @@ final class ParserTests: XCTestCase {
             FunctionDefinition(name=cube, args=[(x, Int)], returnType=null, body=[Multiply [Multiply [Ref x] [Ref x]] [Ref x]])
             """)
     }
-    
+
     private func assertSyntaxError(code: String) {
         var thrownError: Error?
         XCTAssertThrowsError(try parseExpression(code: code), "expected syntax error") {
@@ -141,28 +153,30 @@ final class ParserTests: XCTestCase {
         }
         XCTAssert(thrownError is SyntaxErrorException)
     }
-    
+
     private func assertParseExpression(source: String, expected: String) throws {
         let expression = try parseExpression(code: source)
-        
+
         XCTAssertEqual(expected, expression.description, source)
     }
-    
+
     private func assertParseFunctionDefinition(source: String, expected: String) throws {
         let expression = try parseFunctionDefinition(code: source)
-        
+
         XCTAssertEqual(expected, expression.description, source)
     }
-    
+
     static var allTests = [
         ("testVariables", testVariables),
         ("testLiterals", testLiterals),
         ("testIfStatements", testIfStatements),
+        ("testUnlessStatements", testUnlessStatements),
         ("testWhileStatements", testWhileStatements),
         ("testAssignment", testAssignment),
         ("testVars", testVars),
         ("testVals", testVals),
         ("testIfAsAnExpression", testIfAsAnExpression),
+        ("testUnlessAsAnExpression", testUnlessAsAnExpression),
         ("testExpressionList", testExpressionList),
         ("testAssignmentToLiteralIsSyntaxError", testAssignmentToLiteralIsSyntaxError),
         ("testBinaryOperators", testBinaryOperators),
--- Swona/Tests/SwonaTests/VMTests.swift
+++ Swona/Tests/SwonaTests/VMTests.swift
@@ -4,58 +4,80 @@ import Swona
 
 final class VMTests: XCTestCase {
     let evaluator = Evaluator(trace: false)
-    
+
     func testLiteralEvaluation() throws {
         try assertEvaluation(code: "42", expectedValue: Value.integer(value: 42))
         try assertEvaluation(code: "true", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "\"foo\"", expectedValue: Value.string(value: "foo"))
     }
-    
+
     func testVariableEvaluation() throws {
         try evaluator.bind(name: "x", value: .integer(value: 123))
-    
+
         try assertEvaluation(code: "x", expectedValue: .integer(value: 123))
     }
-    
+
     func testVarStatements() throws {
         try evaluate(code: "var x = 42")
-    
+
         try assertEvaluation(code: "x", expectedValue: .integer(value: 42))
     }
-    
+
     func testAssignments() throws {
         try evaluator.bind(name: "x", value: .integer(value: 42))
-    
+
         try evaluate(code: "x = 123")
-    
+
         try assertEvaluation(code: "x", expectedValue: .integer(value: 123))
     }
-    
+
     func testArithmetic() throws {
         try assertEvaluation(code: "1 + 2 * 3 + 4 / 2", expectedValue: .integer(value: 9))
     }
-    
+
     func testIfExpressions() throws {
         try evaluator.bind(name: "x", value: Value.bool(value: true))
         try evaluator.bind(name: "y", value: .integer(value: 42))
         try evaluator.bind(name: "r", value: .integer(value: 0))
-    
+
         try evaluate(code: "if (x) r = 123 else r = y")
         try assertEvaluation(code: "r", expectedValue: .integer(value: 123))
-    
+
         try evaluate(code: "x = false")
-    
+
         try evaluate(code: "if (x) r = 123 else r = y")
         try assertEvaluation(code: "r", expectedValue: .integer(value: 42))
     }
-    
+
     func testIfExpressionValues() throws {
         try evaluator.bind(name: "x", value: Value.bool(value: true))
-        
+
         try assertEvaluation(code: "if (x) 1 else 2", expectedValue: .integer(value: 1))
         try assertEvaluation(code: "if (!x) 1 else 2", expectedValue: .integer(value: 2))
     }
-    
+
+    func testUnlessExpressions() throws {
+        try evaluator.bind(name: "x", value: Value.bool(value: true))
+        try evaluator.bind(name: "y", value: .integer(value: 42))
+        try evaluator.bind(name: "r", value: .integer(value: 0))
+
+        try evaluate(code: "unless (x) r = 123 else r = y")
+        try assertEvaluation(code: "r", expectedValue: .integer(value: 42))
+
+        try evaluate(code: "x = false")
+
+        try evaluate(code: "unless (x) r = 123 else r = y")
+        try assertEvaluation(code: "r", expectedValue: .integer(value: 123))
+    }
+
+
+    func testUnlessExpressionValues() throws {
+        try evaluator.bind(name: "x", value: Value.bool(value: true))
+
+        try assertEvaluation(code: "unless (x) 1 else 2", expectedValue: .integer(value: 2))
+        try assertEvaluation(code: "unless (!x) 1 else 2", expectedValue: .integer(value: 1))
+    }
+
     func testBinaryExpressions() throws {
         try assertEvaluation(code: "1 + 2", expectedValue: .integer(value: 3))
         try assertEvaluation(code: "1 - 2", expectedValue: .integer(value: -1))
@@ -64,21 +86,21 @@ final class VMTests: XCTestCase {
         try assertEvaluation(code: "1 != 2", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "1 != 1", expectedValue: Value.bool(value: false))
     }
-    
+
     func testIfWithoutElse() throws {
         try evaluator.bind(name: "r", value: .integer(value: 0))
-        
+
         try evaluate(code: "if (false) r = 1")
         try assertEvaluation(code: "r", expectedValue: .integer(value: 0))
         try evaluate(code: "if (true) r = 2")
         try assertEvaluation(code: "r", expectedValue: .integer(value: 2))
     }
-    
+
     func testWhileLoop() throws {
         try evaluator.bind(name: "x", value: .integer(value: 5))
         try evaluator.bind(name: "a", value: .integer(value: 0))
         try evaluator.bind(name: "b", value: .integer(value: 0))
-        
+
         try evaluate(code: """
                     while (x != 0) {
                         x = x - 1;
@@ -86,40 +108,40 @@ final class VMTests: XCTestCase {
                         b = a + b
                     }
     """)
-        
-        
+
+
         try assertEvaluation(code: "x", expectedValue: .integer(value: 0))
         try assertEvaluation(code: "a", expectedValue: .integer(value: 5))
         try assertEvaluation(code: "b", expectedValue: .integer(value: 15))
     }
-    
+
     func testNot() throws {
         try evaluate(code: "val x = true")
         try assertEvaluation(code: "!x", expectedValue: .bool(value: false))
         try assertEvaluation(code: "!!x", expectedValue: .bool(value: true))
     }
-    
+
     func testEvaluationFailuresForCoercions() throws {
         assertTypeCheckFails(s: "1 + \"foo\"")
         assertTypeCheckFails(s: "!1")
     }
-    
+
     func testDirectCalls() throws {
         try defineSquareFunction()
         try assertEvaluation(code: "square(4)", expectedValue: .integer(value: 16))
     }
-    
+
     func testFunctionCallsThroughLocalVariable() throws {
         try defineSquareFunction()
         try evaluator.bind(name: "result", value: .integer(value: 0))
-    
+
         try evaluate(code: """
             if (true) {
                 var sq = square;
                 result = sq(5)
             }
     """)
-    
+
         try assertEvaluation(code: "result", expectedValue: .integer(value: 25))
     }
 
@@ -127,23 +149,23 @@ final class VMTests: XCTestCase {
         try defineSquareFunction()
         try assertEvaluation(code: "(square)(6)", expectedValue: .integer(value: 36))
     }
-    
+
     func testExpressionFunctions() throws {
         try evaluate(code:"fun sub(x: Int, y: Int): Int = x - y")
         try assertEvaluation(code: "sub(7, 4)", expectedValue: .integer(value: 3))
     }
-    
+
     func testEvaluationFailsForUnboundVariables() throws {
         assertTypeCheckFails(s: "x")
         assertTypeCheckFails(s: "x = 4")
     }
-    
+
     func testLogicalOperators() throws {
         try assertEvaluation(code: "false || false", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "false || true", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "true || false", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "true || true", expectedValue: Value.bool(value: true))
-        
+
         try assertEvaluation(code: "false && false", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "false && true", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "true && false", expectedValue: Value.bool(value: false))
@@ -153,13 +175,13 @@ final class VMTests: XCTestCase {
     func testEvaluationFailsForRebindingVariables() throws {
         assertTypeCheckFails(s: "{ var x = 4; var x = 4 }")
     }
-    
+
     func testPlusWithStringLiteralOnLeftSideIsStringConcatenation() throws {
         try assertEvaluation(code: "\"foo \" + \"bar\"", expectedValue: Value.string(value: "foo bar"))
         try assertEvaluation(code: "\"foo \" + 42", expectedValue: Value.string(value: "foo 42"))
         try assertEvaluation(code: "\"foo \" + true", expectedValue: Value.string(value: "foo true"))
     }
-    
+
     func testRelationalOperators() throws {
         try assertEvaluation(code: "1 == 1", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "1 != 1", expectedValue: Value.bool(value: false))
@@ -167,7 +189,7 @@ final class VMTests: XCTestCase {
         try assertEvaluation(code: "1 <= 1", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "1 > 1", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "1 >= 1", expectedValue: Value.bool(value: true))
-        
+
         try assertEvaluation(code: "1 == 2", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "1 != 2", expectedValue: Value.bool(value: true))
         try assertEvaluation(code: "1 < 2", expectedValue: Value.bool(value: true))
@@ -175,28 +197,28 @@ final class VMTests: XCTestCase {
         try assertEvaluation(code: "1 > 2", expectedValue: Value.bool(value: false))
         try assertEvaluation(code: "1 >= 2", expectedValue: Value.bool(value: false))
     }
-    
+
     func testNativeFunctionCallWithSingleParameter() throws {
-        
+
         try evaluator.bind(name: "inc",  value: .function(fun1(name: "inc", argType: Type.int, returnType: Type.int) { (value: Value) -> Value in
             return value.plus(rhs: .integer(value: 1))}), mutable: false)
-    
+
         try assertEvaluation(code: "inc(4)", expectedValue:.integer(value: 5))
     }
-    
+
     func testNativeFunctionCallWithMultipleParameters() throws {
-        
+
         try evaluator.bind(name: "sub",  value: .function(fun2(name: "sub", argType: Type.int, returnType: Type.int) { (a: Value, b: Value) -> Value in
             return a.minus(rhs: b)}), mutable: false)
-        
+
         try assertEvaluation(code: "sub(7, 4)", expectedValue:.integer(value: 3))
     }
 
-    
+
     func testNestedIfs() throws {
         try assertEvaluation(code: "if (false) 1 else if (true) 2 else 3", expectedValue: .integer(value: 2))
     }
-    
+
     func testRecursion() throws {
         try evaluate(code: """
             fun fib(i: Int): Int =
@@ -207,12 +229,12 @@ final class VMTests: XCTestCase {
                 else
                     fib(i-1) + fib(i-2)
     """)
-        
+
         try assertEvaluation(code: "fib(2)", expectedValue: .integer(value: 1))
         try assertEvaluation(code: "fib(10)", expectedValue: .integer(value: 55))
         try assertEvaluation(code: "fib(20)", expectedValue: .integer(value: 6765))
     }
-    
+
     func testRuntimeFunctions() throws {
         try registerRuntimeFunctions(evaluator: evaluator)
         try evaluate(code: #"var a = stringArrayOfSize(3, "")"#)
@@ -223,7 +245,7 @@ final class VMTests: XCTestCase {
         try assertEvaluation(code: "stringArrayGet(a, 1)", expectedValue: .string(value: "1"))
         try assertEvaluation(code: "stringArrayGet(a, 2)", expectedValue: .string(value: "2"))
     }
-    
+
     private func assertTypeCheckFails(s: String) {
         var thrownError: Error?
         XCTAssertThrowsError(try evaluate(code: s), "expected type check exception") {
@@ -231,14 +253,14 @@ final class VMTests: XCTestCase {
         }
         XCTAssert(thrownError is TypeCheckException)
     }
-    
+
     private func assertEvaluation(code: String, expectedValue: Value) throws {
         evaluator.optimize = true
         XCTAssertEqual(expectedValue, try evaluate(code: code))
         evaluator.optimize = false
         XCTAssertEqual(expectedValue, try evaluate(code: code))
     }
-    
+
     private func defineSquareFunction() throws {
         try evaluator.evaluate(code: "fun square(x: Int) = x * x")
     }
@@ -256,6 +278,8 @@ final class VMTests: XCTestCase {
         ("testArithmetic", testArithmetic),
         ("testIfExpressions", testIfExpressions),
         ("testIfExpressionValues", testIfExpressionValues),
+        ("testUnlessExpressions", testIfExpressions),
+        ("testUnlessExpressionValues", testIfExpressions),
         ("testBinaryExpressions", testBinaryExpressions),
         ("testIfWithoutElse", testIfWithoutElse),
         ("testWhileLoop", testWhileLoop),
